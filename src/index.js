b63700687b87224d000878c4054d46637f6a0b00
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2018-12-12 01:52:07 +0300

    implement solution

--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
+import { uniq, has } from 'lodash';
+import { readFileSync } from 'fs';
+
+const beforeView = (key, value) => `  - ${key}: ${value}`;
+const afterView = (key, value) => `  + ${key}: ${value}`;
+const unchgView = (key, value) => `    ${key}: ${value}`;
+
 const diffCalc = k => (b, a) => {
+  if (!has(b, k)) {
+    return afterView(k, a[k]);
+  }
+  if (!has(a, k)) {
+    return beforeView(k, b[k]);
+  }
   if (b[k] === a[k]) {
-    return `  ${k}: ${a[k]}`;
+    return unchgView(k, b[k]);
   }
-  if (b[k] === undefined) {
-    return `+ ${k}: ${a[k]}`;
-  }
-  if (a[k] === undefined) {
-    return `- ${k}: ${b[k]}`;
-  }
-  return `+ ${k}: ${a[k]}\n- ${k}: ${b[k]}`;
+  return [afterView(k, a[k]), beforeView(k, b[k])].join('\n');
 };
 
-const genDiff = (before, after) => {
-  const beforeObject = JSON.parse(before);
-  const afterObject = JSON.parse(after);
-  const selector = Object.keys(beforeObject).concat(Object.keys(afterObject))
-    .reduce((acc, k) => ({ ...acc, [k]: diffCalc(k) }), {});
-  return `{\n${Object.keys(selector)
-    .map(k => selector[k](beforeObject, afterObject)).join('\n')}\n}`;
+const plainJson = (before, after) => {
+  const beforeData = readFileSync(before, 'utf-8');
+  const afterData = readFileSync(after, 'utf-8');
+  const beforeObject = JSON.parse(beforeData);
+  const afterObject = JSON.parse(afterData);
+  const mapper = uniq(Object.keys(beforeObject).concat(Object.keys(afterObject)))
+    .map(k => diffCalc(k));
+  return ['{', ...mapper.map(fn => fn(beforeObject, afterObject)), '}'].join('\n');
 };
-export default genDiff;
+export default plainJson;
