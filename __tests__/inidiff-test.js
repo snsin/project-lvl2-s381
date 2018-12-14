2622729df2c076458d2529930df42923f90bae7c
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2018-12-14 09:20:47 +0300

    removed old version of tests

--- __tests__/inidiff-test.js
+++
@@ -1,9 +0,0 @@
-import { readFileSync } from 'fs';
-import iniDiff from '../src';
-
-test('.ini files test', () => {
-  const before = '__tests__/__fixtures__/before.ini';
-  const after = '__tests__/__fixtures__/after.ini';
-  const data = readFileSync('__tests__/__fixtures__/b-a-diff.txt', 'utf-8');
-  expect(iniDiff(before, after)).toBe(data);
-});
